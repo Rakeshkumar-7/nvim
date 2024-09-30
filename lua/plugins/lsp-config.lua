2ad2096644b5228cb5ce53b6d7a50b33b00e7282
Rakeshkumar-7 <rakeshks7th@gmail.com>
2024-09-30 11:28:51 +0000

    Added: More themes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -37,7 +37,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       mason_lspconfig.setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
+        ensure_installed = { "lua_ls", "ts_ls", "pyright" },
         automatic_installation = true,
       })
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,28 @@
 return {
-  { "EdenEast/nightfox.nvim", lazy = false, name = "theme",      priority = 1000 },
-  { "catppuccin/nvim",        lazy = false, name = "catppuccin", priority = 1000 },
+  -- For transparent background
+  { "xiyaowong/transparent.nvim", lazy = false,       priority = 1000 },
+  -- Themes
+  { "Tsuzat/NeoSolarized.nvim",   lazy = false,       priority = 1000 },
+  { "morhetz/gruvbox",            lazy = false,       priority = 1000 },
+  { "EdenEast/nightfox.nvim",     lazy = false,       name = "theme",      priority = 1000 },
+  { "catppuccin/nvim",            lazy = false,       name = "catppuccin", priority = 1000 },
+  { "rose-pine/neovim",           name = "rose-pine", priority = 1000 },
+  -- Theme selector
   {
     "https://github.com/zaldih/themery.nvim",
     config = function()
       local themery = require("themery")
       themery.setup({
         themes = {
+          {
+            name = "peachpuff",
+            colorscheme = "peachpuff",
+          },
+
+          {
+            name = "ron",
+            colorscheme = "ron",
+          },
           {
             name = "carbonfox",
             colorscheme = "carbonfox",
@@ -18,6 +34,26 @@ return {
           {
             name = "catppuccin",
             colorscheme = "catppuccin",
+          },
+          {
+            name = "rose pine main",
+            colorscheme = "rose-pine-main",
+          },
+          {
+            name = "rose pine moon",
+            colorscheme = "rose-pine-moon",
+          },
+          {
+            name = "rose pine dawn",
+            colorscheme = "rose-pine-dawn",
+          },
+          {
+            name = "NeoSolarized",
+            colorscheme = "NeoSolarized",
+          },
+          {
+            name = "gruvbox",
+            colorscheme = "gruvbox",
           },
         },
         livePreview = true,
